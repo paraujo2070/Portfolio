c03f2147b85d10cd0a41419c506e23f3ebe66f1d
Pedro Araujo <paraujo20700@gmail.com>
2026-02-21 14:23:34 -0300

    Criando página de contato

---
+++ portfolio-pedro/src/app/components/contato/contato.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato',
+  standalone: true,
+  templateUrl: './contato.html'
+})
+export class Contato {
+  email = 'pedro_a_araujo@ufms.br'; 
+  
+  linkedin = 'https://www.linkedin.com/in/pedro-antonio-araújo/';
+  github = 'https://github.com/paraujo2070';
+}
